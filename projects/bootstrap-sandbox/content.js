eae19a9d2b8683cfe5f0c2feea4b4e24a64d5577
NureddinFarzaliyev <nureddin.fa@gmail.com>
2024-11-16 17:40:23 +0400

    translations

--- projects/bootstrap-sandbox/content.js
+++ projects/bootstrap-sandbox/content.js
@@ -1,7 +1,10 @@
-export const data = {
+export const LANGUAGES = {
+    EN: "EN",
+    AZ: "AZ"
+}
 
+export const data = {
     en: {
-        langBtnContent: 'AZ',
         nodeLists: {
             'nav>div>a>span' : {
                 0: "Demos",
@@ -24,23 +27,86 @@ export const data = {
                 1: 'Learn More',
                 2: 'Learn More',
                 3: 'Learn More',
+            },
+            ".intro h2": {
+                0: 'Content Marketing',
+                1: "Social Engagement",
+                2: "Identity & Branding",
+                3: "Product Design"
+            },
+            ".reasons h5": {
+                0: "Collect Ideas",
+                1: "Data Analysis",
+                2: "Finalize Product"
+            },
+            ".stat-p": {
+                0: "Customer Satisfaction",
+                1: "Increased Revenue"
+            },
+            ".grid-section h5": {
+                0: "Creativity",
+                1: "Innovative Thinking",
+                2: "Rapid Solutions",
+                3: "Top-Notch Support"
+            },
+            "footer h5": {
+                0: "Get in Touch",
+                1: "Learn More",
+                2: "Our Newsletter"
+            },
+            "footer p": {
+                0: "© 2024 Sandbox.",
+                1: "All Rights Reserved",
+                2: "Moonshine St. 14/05",
+                3: "London",
+                4: "United Kingdom",
+                5: "info@email.com",
+                6: "00 (123) 456 78 90",
+                7: "About Us",
+                8: "Our Story",
+                9: "Projects",
+                10: "Terms of Use",
+                11: "Privacy Policy",
+                12: "Subscribe to our newsletter to get our news & deals delivered to you."
             }
         },
         elements : {
-            intro: {
-                '.intro.container h1': "We bring solutions to make life easier.",
-                '.intro.container p': "We are a creative company that focuses on long term relationships with customers.",
-                '.intro.container button': "Read More",
-                '.intro.container .cards .col:nth-child(1) h2': 'Content Marketing',
-                '.intro.container .cards .col:nth-child(2) h2': "Social Engagement",
-                '.intro.container .cards .col:nth-child(3) h2': "Identity & Branding",
-                '.intro.container .cards .col:nth-child(4) h2': "Product Design"
-            },
+            // Intro Section
+            '#lang-btn': LANGUAGES.AZ,
+            '.intro.container h1': "We bring solutions to make life easier.",
+            '.intro.container p': "We are a creative company that focuses on long term relationships with customers.",
+            '.intro.container button': "Read More",
+
+            // Reasons Section
+            '.reasons > p': "WHY CHOOSE SANDBOX?",
+            '.reasons > h1': "Here are a few reasons why our customers choose Sandbox.",
+            '.reasons h3': "Collect Ideas",
+            '.reasons button': "Learn More",
+
+            // Professional section
+            '.professional .statistics-header > p': "SAVE TIME AND MONEY",
+            '.professional .statistics-header > h1': "Save your time and money by choosing our professional team.",
+            '.professional .review': 'Marketing Specialist',
+
+            // Projects Section
+            '.slider-section > div > p': 'LATEST PROJECTS',
+            '.slider-section h1': 'Check out some of our awesome projects with creative ideas and great design.',
+
+            // Grid Section
+            '.grid-section .opaque-header': 'WHAT MAKES US DIFFERENT?',
+            '.grid-section h1': 'We bring solutions to make life easier for our customers.',
+            
+            // Community Section
+            '.community p': "JOIN OUR COMMUNITY",
+            '.community h2': "We are trusted by over 5000+ clients. Join them now and grow your business.",
+            '.community button': 'Get Started',
+
+            // Footer Section
+            'footer button': "Join",
         }
     },
 
     az: {
-        langBtnContent: 'EN',
         nodeLists: {
             'nav>div>a>span' : {
                 0: "Demolar",
@@ -63,22 +129,82 @@ export const data = {
                 1: 'Ətraflı Öyrən',
                 2: 'Ətraflı Öyrən',
                 3: 'Ətraflı Öyrən',
+            },
+            ".intro h2": {
+                0: 'Kontent Marketinq',
+                1: "Sosial Əlaqələr",
+                2: "Şəxsiyyət və Brendinq",
+                3: "Məhsul Dizaynı"
+            },
+            ".reasons h5": {
+                0: "Ideaya Toplamaq",
+                1: "Data Analizi",
+                2: "Məhsulu Yekunlaşdırmaq"
+            },
+            ".stat-p": {
+                0: "Müştəri Məmnunluğu",
+                1: "Gəlir Artışı"
+            },
+            ".grid-section h5": {
+                0: "Yaradıcılıq",
+                1: "İnnovativ Düşüncə",
+                2: "Cəld Həllər",
+                3: "Keyfiyyətli Dəstək"
+            },
+            "footer h5": {
+                0: "Əlaqəyə Keç",
+                1: "Məlumat",
+                2: "Xəbər Lentimiz"
+            },
+            "footer p": {
+                0: "© 2024 Sandbox.",
+                1: "Bütün Hüquqlar Qorunur",
+                2: "Moonshine St. 14/05",
+                3: "London",
+                4: "United Kingdom",
+                5: "info@email.com",
+                6: "00 (123) 456 78 90",
+                7: "Haqqımızda",
+                8: "Hekayəmiz",
+                9: "Proyektlər",
+                10: "İstifadə Qaydaları",
+                11: "Gizlilik Siyasəti",
+                12: "Yenilik və xəbərlərimizi sizə çatdırmağımız üçün xəbər lentimizə abunə olun.",
             }
         },
         elements: {
-            intro: {
-                '.intro.container h1': "Həyatı asanlaşdıracaq həllər gətiririk.",
-                '.intro.container p': "Biz müştərilərlə uzunmüddətli əlaqələrə fokuslanmış yaradıcı şirkətik.",
-                '.intro.container button': "Ətraflı oxu",
-                '.intro.container .cards .col:nth-child(1) h2': 'Kontent Marketinq',
-                '.intro.container .cards .col:nth-child(2) h2': "Sosial Əlaqələr",
-                '.intro.container .cards .col:nth-child(3) h2': "Şəxsiyyət və Brendinq",
-                '.intro.container .cards .col:nth-child(4) h2': "Məhsul Dizaynı"
-            },
+            // Intro Section
+            '#lang-btn': LANGUAGES.EN,
+            '.intro.container h1': "Həyatı asanlaşdıran həllər gətiririk.",
+            '.intro.container p': "Biz müştərilərlə uzunmüddətli əlaqələrə fokuslanmış yaradıcı şirkətik.",
+            '.intro.container button': "Ətraflı oxu",
+
+            // Reasons Section 
+            '.reasons > p': "NİYƏ SANDBOXU SEÇMƏLİ?",
+            '.reasons > h1': "Müştərilərimizin Sandboxu seçməsi üçün bir neçə səbəb.",
+            '.reasons h3': "İdeyalar Topla",
+            '.reasons button': "Ətraflı Öyrən",
+
+            // Professional Section
+            '.professional .statistics-header > p': "PULA VƏ VAXTA QƏNAƏT",
+            '.professional .statistics-header > h1': "Peşəkar komandamızı seçməklə vaxtınıza və pulunuza qənaət edin.",
+            '.professional .review': 'Marketinq Mütəxəssisi',
+
+            // Projects Section
+            '.slider-section > div > p': 'YENI PROYEKTLƏR',
+            '.slider-section h1': 'Mükəmməl dizayn və kreativ ideyalarla dolu proyektlərimizə nəzər yetirin.',
+
+            // Grid Section
+            '.grid-section .opaque-header': 'BİZİ FƏRQLİ EDƏN NƏDİR?',
+            '.grid-section h1': 'Müştərilərimizin həyatını asanlaşdıran həllər təqdim edirik.',
+
+            // Community Section
+            '.community p': "CƏMİYYƏTİMİZƏ QOŞUL",
+            '.community h2': "5000+ müştəri bizə güvənir. Onlara qoşul və biznesini inkişaf etdir.",
+            '.community button': 'Başla',
+
+            // Footer Section
+            'footer button': "Qoşul",
         }
     }
 }
-
-export const arr = ['a', 'b']
-
-// export default {data}
--- projects/bootstrap-sandbox/script.js
+++ projects/bootstrap-sandbox/script.js
@@ -1,37 +1,20 @@
-import { data } from "./content.js";
-// console.log(data)
+import { LANGUAGES, data } from "./content.js";
 
 const langBtn = document.querySelector('#lang-btn');
 
-const translateNodeList = (list, arr) => {
-    list.forEach((item, index) => {
-        item.textContent = arr[index]
-    })
-}
-
-const translateElement = (element, text) => {
-    element.textContent = text
-}
-
 const setLanguage = (lang) => {
-    const {langBtnContent, elements, nodeLists } = data[lang.toLowerCase()]
+    const {elements, nodeLists} = data[lang.toLowerCase()]
 
-    for(let [_, obj] of Object.entries(elements)){
-        for(let [key, value] of Object.entries(obj)){
-            translateElement(document.querySelector(key), value)
-        }
+    for(let [selector, text] of Object.entries(elements)){
+        document.querySelector(selector).textContent = text
     }
 
-    for(let [key, value] of Object.entries(nodeLists)){
-        translateNodeList(document.querySelectorAll(key), value)
+    for(let [query, nodeList] of Object.entries(nodeLists)){
+        document.querySelectorAll(query).forEach((item, index) => {
+            item.textContent = nodeList[index]
+        })
     }
-
-    langBtn.textContent = langBtnContent;
-
 }
 
-langBtn.addEventListener('click', () => {
-    setLanguage(langBtn.textContent)
-})
-
-setLanguage("en")
+langBtn.addEventListener('click', () => {setLanguage(langBtn.textContent)})
+setLanguage(LANGUAGES.EN)
