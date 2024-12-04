fb9341da7b5213960a747b13432de3bc091287c2
NureddinFarzaliyev <nureddin.fa@gmail.com>
2024-12-04 08:32:07 +0400

    fetch books and add/remove wishlist

--- projects/movie-book-app/src/js/main.js
+++ projects/movie-book-app/src/js/main.js
@@ -44,8 +44,74 @@ const getMovieData = (id) => {
     .catch(err => console.error(err));
 }
 
+const checkIfBookInWishlist = (bookId, addBtn, removeBtn) => {
+    const wishlist = JSON.parse(localStorage.getItem('books'))
+    if(wishlist && wishlist.includes(bookId)) {
+        addBtn.classList.add('hidden')
+        removeBtn.classList.remove('hidden')
+    }else{
+        addBtn.classList.remove('hidden')
+        removeBtn.classList.add('hidden')
+    }
+}
+
+const addBooksToPage = (bookArr, container) => {
+    container.innerHTML = ''
+    bookArr.forEach(book => {
+        const bookCard = document.createElement('div')
+        
+        bookCard.innerHTML = `
+        <h1 class="bg-amber-800">${book.title}</h1>
+        `
+
+        const addBtn = document.createElement('button')
+        addBtn.innerHTML = 'Add to list'
+
+        const removeBtn = document.createElement('button')
+        removeBtn.innerHTML = 'Remove from list'
+
+        addBtn.addEventListener('click', () => {
+            const currentBooks = JSON.parse(localStorage.getItem('books'))
+            localStorage.setItem('books', currentBooks ? JSON.stringify([...currentBooks, book.key]) : JSON.stringify([book.key]))
+            checkIfBookInWishlist(book.key, addBtn, removeBtn)
+        })
+
+        removeBtn.addEventListener('click', () => { 
+            const currentBooks = JSON.parse(localStorage.getItem('books'))
+            localStorage.setItem('books', JSON.stringify(currentBooks.filter(item => item !== book.key)))
+            checkIfBookInWishlist(book.key, addBtn, removeBtn)
+        })
+
+        
+        bookCard.appendChild(removeBtn)
+        bookCard.appendChild(addBtn)
+
+        checkIfBookInWishlist(book.key, addBtn, removeBtn)
+        
+        container.appendChild(bookCard)
+        
+    }) 
+}
+
+const recommendBooks = (query, container) => {
+    container.innerHTML = 'Books Loading'
+    const url = `https://openlibrary.org/search.json?q=${query}&limit=5`
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+        }
+    };
+
+    fetch(url, options)
+    .then(res => res.json())
+    .then(json => addBooksToPage(json.docs, container))
+    .catch(err => console.error(err));
+}
+
 const currentMovieContainer = document.querySelector('.movie-info')
 const currentMovieData = document.querySelector('.movie-data')
+const currentBookData = document.querySelector('.book-data')
 
 const showAddBtn = () => {
     document.querySelector('.movie-info button.add').classList.remove('hidden')
@@ -58,12 +124,20 @@ const showRemoveBtn = () => {
 }
 
 const showDetailedMovieData = (movieObj) => {
+    console.log(movieObj)
     currentMovieContainer.classList.remove('hidden')
     currentMovieContainer.setAttribute('data-id', movieObj.id)
     
     const currentMovies = JSON.parse(localStorage.getItem('movies'))
     if(currentMovies.every((item) => item != movieObj.id)) showAddBtn()
     else showRemoveBtn()
+
+    let bookQuery = ''
+    for(let i = 0; i < movieObj.genres.length; i++) {
+        bookQuery += movieObj.genres[i].name + (i === movieObj.genres.length - 1 ? '' : '+')
+    }
+
+    recommendBooks(bookQuery, currentBookData)
 
     currentMovieData.innerHTML = `${movieObj.title}`
 }
