2881ad8adc3f45c0627edb1fa1b885aa06b19067
NureddinFarzaliyev <nureddin.fa@gmail.com>
2024-12-17 12:39:43 +0400

    counter

--- react/counter/src/App.jsx
+++ react/counter/src/App.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      counter: 0
+    }
+  }
+
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  decrement = () => {
+    this.setState({
+      counter: this.state.counter - 1 <= 0 ? 0 : this.state.counter - 1 
+    })
+  }
+
   render() {
     return (
-      <div>
-        
+      <div className='container'>
+        <h1 className={this.state.counter <= 0 ? 'no-text-glow' : ''}>{this.state.counter}</h1>
+        <div>
+          <button onClick={this.increment}>+</button>
+          <button onClick={this.decrement} disabled={this.state.counter <= 0}>-</button>
+        </div>
       </div>
     )
   }
