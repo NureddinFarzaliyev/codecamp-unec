f3a0498075554a33fd898217a734b29b7d84d3fe
NureddinFarzaliyev <nureddin.fa@gmail.com>
2024-12-19 08:14:44 +0400

    fixes for mobile nav

--- react/bookapp/src/components/nav/MobileNav.jsx
+++ react/bookapp/src/components/nav/MobileNav.jsx
@@ -8,7 +8,7 @@ class MobileNav extends Component {
 
   render() {
     return (
-        <div className='bg-white border-opacity-80 border-r-4 gap-3 absolute h-[100vh] w-72 left-0 top-0 flex flex-col pt-10 px-5 shadow-2xl z-[999]'>
+        <div className='bg-white border-opacity-80 border-r-4 gap-3 absolute h-[100vh] w-72 left-0 top-0 flex flex-col pt-10 px-5 shadow-2xl'>
             {this.navLinks.map((link, index) => <div key={index}><NavLink text={link} /> <hr /></div>)}
             <LoginBtn />
         </div>
--- react/bookapp/src/components/nav/Nav.jsx
+++ react/bookapp/src/components/nav/Nav.jsx
@@ -17,6 +17,7 @@ class Nav extends Component {
 
   changeMobileNav = () => {
     this.setState({showMobileNav: !this.state.showMobileNav})
+    document.body.style.overflow = this.state.showMobileNav === true ? 'auto' : 'hidden'
   } 
 
   render() {
@@ -24,8 +25,8 @@ class Nav extends Component {
       <Container>
       <div className='flex items-center justify-between h-20'>
         <img src={Logo} alt="" /> <NavList /> <LoginBtn hideOnMobile={true} />
-        <button onClick={this.changeMobileNav} className='flex xl:hidden h-9 w-9 bg-accent text-2xl text-white items-center justify-center'>{this.state.showMobileNav === true ? <RxCross1/> : <RxHamburgerMenu />}</button>
-        <div className={`absolute top-0 left-0 ${this.state.showMobileNav === true ? 'come-in' : 'get-out'}`}><MobileNav /></div>
+        <button onClick={this.changeMobileNav} className='flex xl:hidden h-9 w-9 bg-accent text-2xl text-white items-center justify-center z-[1000]'>{this.state.showMobileNav === true ? <RxCross1/> : <RxHamburgerMenu />}</button>
+        <div className={`absolute top-0 left-0 z-[100] ${this.state.showMobileNav === true ? 'come-in overlay' : 'get-out'}`}><MobileNav /></div>
       </div>
       </Container>
     )
