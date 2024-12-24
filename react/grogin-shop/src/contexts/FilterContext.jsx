039617315f8b904b58217971716bc1c840f96e1f
NureddinFarzaliyev <nureddin.fa@gmail.com>
2024-12-24 18:02:04 +0400

    category functionality

---
+++ react/grogin-shop/src/contexts/FilterContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+export const ActiveFilterContext = createContext();
+export const ActiveFilterDispatch = createContext();
+export const FilterTypeContext = createContext();
+export const FilterTypeDispatch = createContext();
---
+++ react/grogin-shop/src/contexts/FilterProvider.jsx
@@ -0,0 +1,22 @@
+import { ActiveFilterContext, ActiveFilterDispatch, FilterTypeContext, FilterTypeDispatch } from "./FilterContext"
+import { useState } from "react"
+
+const FilterProvider = ({children}) => {
+
+  const [activeFilter, setActiveFilter] = useState('Dessert')
+  const [filterType, setFilterType] = useState('c')
+
+  return (
+    <ActiveFilterContext.Provider value={activeFilter}>
+      <ActiveFilterDispatch.Provider value={setActiveFilter}>
+        <FilterTypeContext.Provider value={filterType}>
+          <FilterTypeDispatch.Provider value={setFilterType}>
+            {children}
+          </FilterTypeDispatch.Provider>
+        </FilterTypeContext.Provider>
+      </ActiveFilterDispatch.Provider>
+    </ActiveFilterContext.Provider>
+  )
+}
+
+export default FilterProvider
